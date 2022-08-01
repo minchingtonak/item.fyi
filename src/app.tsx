fbda5b6f7972945e8504878b2c23689ae5c42dc6
Alec Minchington <minchingtonak@users.noreply.github.com>
2022-07-31 21:24:47 -0400

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-2 h-screen w-full">
+      <p class="font-bold text-3xl">Hello Vite + Preact!</p>
+      <a
+        class="font-mono underline text-blue-700 text-xl"
+        href="https://preactjs.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Preact
+      </a>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+import checker from 'vite-plugin-checker';
+import alias from '@rollup/plugin-alias';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    preact(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        alias({
+          entries: [
+            { find: 'react', replacement: 'preact/compat' },
+            { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+            { find: 'react-dom', replacement: 'preact/compat' },
+            { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+          ],
+        }),
+      ],
+    },
+  },
+});
